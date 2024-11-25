b5e93677b9b8659c57983e4353d6d9778ea05fb8
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-11-25 14:37:53 +0900

    D. 모든회원 조회, 회원추가 키몸무게 입력 버그 해결, 클래스 파일 키 <-> 몸무게 순서 변경

--- test.py
+++ test.py
@@ -38,7 +38,7 @@ def init_member():
         print("\n체중은 숫자만 입력 가능합니다.\n")
         return
     
-    return Member(id, sex, weight, height)
+    return Member(id, sex, height, weight)
 
 def edit_member():
     if len(member_list) == 0:
@@ -49,7 +49,7 @@ def edit_member():
         id = input("아이디: ").strip().upper()
         for member in member_list:
             if member.id == id:
-                print(f"현재 신장 : {member.height}m")
+                print(f"현재 신장 : {member.height:.1f}m")
                 edit_height = input("수정 신장(m): ")
                 if edit_height == "" or edit_height == None:
                     pass
@@ -59,7 +59,7 @@ def edit_member():
                     except Exception:
                         print("\n신장은 숫자만 입력 가능합니다.\n")
                         return
-                print(f"현재 체중 : {member.weight}kg")
+                print(f"현재 체중 : {member.weight:.1f}kg")
                 edit_weight = input("수정 체중(kg): ")
                 if edit_weight == "" or edit_weight == None:
                     pass
@@ -75,20 +75,45 @@ def edit_member():
                 print("\n존재하지 않는 아이디입니다.\n")
                 return
     
+def print_member():
+    if len(member_list) == 0:
+        print("\n등록된 회원이 없습니다.\n")
+        return
+    else:
+        print("[전체 상태 조회]")
+        print("=" * 20)
+        male_members = [m for m in member_list if m.sex == "M"]
+        female_members = [m for m in member_list if m.sex == "F"]
+        
+        if len(male_members) > 0:
+            print("[남성]")
+            for i, member in enumerate(male_members):
+                print(f"[{i}] 아이디:{member.id} 신장:{member.height:.2f} 체중:{member.weight:.2f} BMI:{member.calc_bmi():.2f}")
+                print(f"도표: {'*' * int(member.calc_bmi())}")
+                
+        if len(female_members) > 0:
+            print("[여성]") 
+            for i, member in enumerate(female_members):
+                print(f"[{i+1}] 아이디:{member.id} 신장:{member.height:.2f} 체중:{member.weight:.2f} BMI:{member.calc_bmi():.2f}")
+                print(f"도표: {'*' * int(member.calc_bmi())}")
+        
+        print("=" * 20)
+        print(f"총 {len(member_list)}명의 정보입니다.")
+        
 while True:
     select = select_menu()
     if select == 'A':
         pass
     elif select == 'B':
         member = init_member()
-        print(f"BMI : {member.calc_bmi()}")
         if member:
             member_list.append(member)
+            print(f"BMI : {member.calc_bmi():.2f}")
         
     elif select == 'C':
         edit_member()
     elif select == 'D':
-        pass
+        print_member()
     elif select == 'Q':
         pass
         break
--- test_class.py
+++ test_class.py
@@ -1,16 +1,17 @@
 class Member:
-    def __init__(self, id, sex, weight, height):
+    def __init__(self, id, sex, height, weight):
         self.id = id
         self.sex = sex
-        self.weight = weight
         self.height = height
+        self.weight = weight
         
     def calc_bmi(self):
         return self.weight / (self.height * self.height)
 
     def retrun_string(self):
-        return f"{self.id}|{self.sex}|{self.weight}|{self.height} "
+        return f"{self.id}|{self.sex}|{self.height}|{self.weight} "
     
     def __str__(self): # 테스트 코드 (필요한것 아님)
-        return f"{self.id}|{self.sex}|{self.weight}|{self.height} "
+        return f"{self.id}|{self.sex}|{self.height}|{self.weight} "
+    
     
